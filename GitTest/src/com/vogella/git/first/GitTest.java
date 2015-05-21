282556d6e0c70f862839f48cde4741ef484b66d9
Oleksandr <alexanchik@mail.com>
2015-05-21 00:38:23 -0400

    created new branch with new variable

--- GitTest/src/com/vogella/git/first/GitTest.java
+++ GitTest/src/com/vogella/git/first/GitTest.java
@@ -8,7 +8,7 @@ public class GitTest
 			double gallons;//holds the number of gallons
 			double liters; // holds conversion to liters
 			
-			gallons = 10; // start with 10 gallons
+			gallons = 100; // start with 10 gallons
 			
 			liters = gallons * 3.7854; //convert to liters
 			
